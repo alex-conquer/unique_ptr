9480f84af0813c80196cd2995fd4ee0bfb85f8a5
alex <a.tymin@bk.ru>
2026-09-23 18:13:32 +0300

    [feat] add move operations

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 
 template <typename T>
 struct controlBlock {
@@ -27,6 +28,35 @@ public:
         std::cout << "copy constructor" << std::endl;
     }
 
+    shared_ptr& operator=(const shared_ptr& other) {
+        if (&other != this) {
+            if (block && --block->counter == 0) {
+                delete block->pointer;
+                delete block;
+            }
+            block = other.block;
+            block->counter++;
+        }
+        std::cout << "operator=" << std::endl;
+        return *this;
+    }
+
+    shared_ptr(shared_ptr&& other) noexcept : block(std::exchange(other.block, nullptr)) {
+        std::cout << "move constructor" << std::endl;
+    }
+
+    shared_ptr& operator=(shared_ptr&& other) noexcept {
+        if (&other != this) {
+            if (block && --block->counter == 0) {
+                delete block->pointer;
+                delete block;
+            }
+            block = std::exchange(other.block, nullptr);
+        }
+        std::cout << "operator=" << std::endl;
+        return *this;
+    }
+
 private:
     controlBlock<T>* block;
 };
@@ -38,5 +68,7 @@ class testClass{
 int main() {
     shared_ptr<testClass> testPointer1(new testClass());
     shared_ptr<testClass> testPointer2 = testPointer1;
+    shared_ptr<testClass> testPointer3 = std::move(testPointer1);
+
     return 0;
 }
